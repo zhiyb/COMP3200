ced1e3beebde24fd99825f42d8482cb023450e0b
Norman Zhi <normanzyb@gmail.com>
2016-04-23 22:23:18 +0000

    OpenCVTegra BGS ViBE Contours

--- OpenCVTegra/bgs_vibe.cpp
+++ OpenCVTegra/bgs_vibe.cpp
@@ -10,10 +10,13 @@
 #define DATASET	"dataset/baseline/highway/"
 
 //#define SHOW
+#define BLOB_SIZE	12
 
 using namespace std;
 using namespace cv;
 using namespace cv::gpu;
+
+static RNG rng(12345);
 
 int main(int argc, char **argv)
 {
@@ -95,11 +98,84 @@ int main(int argc, char **argv)
 #endif
 		vibe(img, fgmask);
 
-#ifdef SHOW
 		Mat mask;
 		fgmask.download(mask);
+		Mat mask_bak = mask.clone();
 
+#ifdef SHOW
 		imshow("mask", mask);
+#endif
+
+#if 1
+		Mat img_mask(mask.size(), mask.type(), Scalar(0.f));
+		{
+			// find blobs
+			std::vector<std::vector<cv::Point> > v;
+			std::vector<cv::Vec4i> hierarchy;
+			cv::findContours(mask, v, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+			for (size_t i=0; i < v.size(); ++i)
+			{
+				// drop smaller blobs
+				if (cv::contourArea(v[i]) < BLOB_SIZE)
+					continue;
+				// draw filled blob
+				cv::drawContours(img_mask, v, i, cv::Scalar(255,0,0), CV_FILLED, 8, hierarchy, 0, cv::Point());
+			}
+
+			// morphological closure
+			cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(BLOB_SIZE, BLOB_SIZE));
+			cv::morphologyEx(img_mask, img_mask, cv::MORPH_CLOSE, element);
+		}
+#else
+		Mat img_mask(mask);
+#endif
+		Mat img_mask_bak = img_mask.clone();
+#ifdef SHOW
+		imshow("img_mask", img_mask);
+#endif
+
+		Mat drawing;
+		img_input.copyTo(drawing);
+#if 1
+		{
+			/// Find contours
+			std::vector<cv::Vec4i> hierarchy;
+			std::vector<std::vector<cv::Point> > contours;
+			//std::vector<cv::Vec4i> hierarchy;
+			cv::findContours(img_mask, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+			/// Approximate contours to polygons + get bounding rects and circles
+			vector<vector<Point> > contours_poly(contours.size());
+			//vector<Rect> boundRect(contours.size());
+			//vector<Point2f>center(contours.size());
+			//vector<float>radius(contours.size());
+
+#if 0
+			for (size_t i = 0; i < contours.size(); i++) {
+				cv::approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+				//boundRect[i] = boundingRect(Mat(contours_poly[i]));
+				//minEnclosingCircle((Mat)contours_poly[i], center[i], radius[i]);
+			}
+#endif
+
+			/// Draw polygonal contour + bonding rects + circles
+			//Mat drawing = Mat::zeros(img_mask.size(), CV_8UC3);
+			for (size_t i = 0; i < contours.size(); i++) {
+				// drop smaller blobs
+				if (cv::contourArea(contours[i]) < BLOB_SIZE)
+					continue;
+
+				//Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
+				Scalar color(255.f, 0.f, 0.f);
+				//cv::drawContours(drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+				cv::drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+				//rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
+				//circle(drawing, center[i], (int)radius[i], color, 2, 8, 0);
+			}
+		}
+#endif
+#ifdef SHOW
+		imshow("drawing", drawing);
 #endif
 
 #if defined(IMGDATASET)
@@ -109,12 +185,11 @@ int main(int argc, char **argv)
 #elif defined(DATASET)
 		if (frameNumber == 1666) {
 #endif
-#ifndef SHOW
-			Mat mask;
-			fgmask.download(mask);
-#endif
-			cv::imwrite("input.png", img_input);
-			cv::imwrite("mask.png", mask);
+			Mat tmp;
+			imwrite("input.png", img_input);
+			imwrite("mask.png", mask_bak);
+			imwrite("img_mask.png", img_mask_bak);
+			imwrite("drawing.png", drawing);
 			//cv::imwrite("bkgmodel.png", img_bkgmodel);
 		}
 		frameNumber++;
